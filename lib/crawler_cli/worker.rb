fc35061f3192d34ebfb3eed26e43c7c81d33b7c2
Stanislav Khovanov <stanislav.khovanov@gmail.com>
2021-03-09 18:28:46 +0300

    add worker class

---
+++ lib/crawler_cli/worker.rb
@@ -0,0 +1,23 @@
+module CrawlerCli
+  class Worker
+    class << self
+      def call(jobs)
+        mutex = Mutex.new
+        results = []
+        threads = []
+
+        jobs.each do |job|
+          threads << Thread.new do
+            message = job.call
+            mutex.synchronize do
+              results << message
+            end
+          end
+        end
+
+        threads.each(&:join)
+        results
+      end
+    end
+  end
+end
